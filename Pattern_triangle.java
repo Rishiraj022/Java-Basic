b0562c5a6fd01d5638e0294966d9954c3e796c54
Rishiraj Potdukhe <115471754+Rishiraj022@users.noreply.github.com>
2023-08-29 10:07:59 +0530

    Add files via upload

---
+++ Pattern_triangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Pattern_triangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		System.out.println("Enter number of rows");
+		num = sc.nextInt();
+		
+		for(int i = 1;i<= num;i++) {
+			for(int j = 1;j<=i;j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+			
+	}
+
+}
