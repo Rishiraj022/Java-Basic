5377c252984399bb9a1eaae5e9c2d37376343d4a
Rishiraj Potdukhe <115471754+Rishiraj022@users.noreply.github.com>
2023-09-01 02:07:13 +0530

    Update student.c

--- student.c
+++ student.c
@@ -1,18 +1,53 @@
+//Name -Rishiraj Potdukhe SEA56
+
 #include<stdio.h>
-using namespace std;
 
 struct Student 
 {
 	int age;
-	int name[30];
+	char name[30];
+};
+
+void getData(struct Student *p){
+	printf("Enter name\t");
+	scanf("%s",p->name);
+	printf("Enter age\t");
+	scanf("%d",&p->age);
+	}
+
+void display(struct Student p){
+    printf("\nName is %s",p.name);
+    printf("\nAge is %d",p.age);
 }
 
-	void getData(struct Student *p){
-		printf("Enter name");
-		scanf("%c",p->name);
-		printf("Enter age");
-		scanf("%d",&p->age);
-	} 
+void search(struct Student s[10]){
+    int temp;
+    printf("\nWhich record you want to search");
+    scanf("%d",&temp);
+
+    display(s[temp]);
+}
+
+void modify(struct Student s[10]){
+    int pos;
+    printf("\nWhich record do you want to modify?");
+    scanf("%d",&pos);
+
+    getData(&s[pos]);
+    
+    printf("The modified record is");
+    display(s[pos]);    
+}
+
+void insert(struct Student s[10],int * ptr){
+    int pos;
+    printf("At which position do you want to insert");
+    scanf("%d",&pos);
+
+    
+
+
+}
 
 void main(){
 	struct Student s[10];
@@ -22,7 +57,15 @@ void main(){
 	
 	for(int i = 0;i < record;i++)
 	{
-		
-	
+		getData(&s[i]);
 	}
+
+    for(int i = 0;i < record;i++)
+	{
+		display(s[i]);
+	}
+
+    search(s);
+    modify(s);
+    insert(s,&record);
 }
